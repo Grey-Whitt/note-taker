6f17fcc9ba9857228811df83cec14121b0b6f3bf
greyw <greywhitt@gmail.com>
2020-10-10 22:17:25 -0500

    added uniqid and fixed deltion

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 const notes = require('../../db/db.json')
 const fs = require("fs");
 const path = require("path");
+const uniqid = require('uniqid');
 
 //writes to the db when new note is added
 function addNote(body, notesArray) {
@@ -22,7 +23,8 @@ router.get('/api/notes', (req, res) => {
 
 //route to post to the db
 router.post('/api/notes', (req, res) => {
-    req.body.id = notes.length
+    //sets the id of each note to an 8 byte unique id based on the current time
+    req.body.id = uniqid.time()
     
     const note = addNote(req.body, notes);
     res.json(note);
@@ -32,15 +34,17 @@ router.post('/api/notes', (req, res) => {
 router.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id
 
-    const newNotes = notes.filter(note => note.id != noteId)
+    const note = notes.findIndex(note => note.id === noteId);
+ 
+    notes.splice(note, 1);
     
+    res.json(noteId)
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(newNotes, null, 2)
+        JSON.stringify(notes, null, 2)
     );
 
 })
-
 
 
 //exports routes so the can be used in server.js
